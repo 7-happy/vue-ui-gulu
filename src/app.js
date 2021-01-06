ecc4bcfabfc3b3dccd27fe3f2ce117a61df50bd5
seeyooagain <652834036@qq.com>
2021-01-06 22:43:56 +0800

    单元测试

--- src/app.js
+++ src/app.js
@@ -1,11 +1,71 @@
 //入口
 import Vue from 'vue'
 import Button from './button'
+import Icon from './icon'
+import ButtonGroup from './button-group'
 
 //全局组件
-Vue.component('g-button',Button)
+Vue.component('g-button',Button);
+Vue.component('g-icon',Icon)
+Vue.component('g-button-group',ButtonGroup)
 
 new Vue({
     el:'#app',
-
+    data:{
+        loading1:false
+    }
 })
+
+
+
+import chai from 'chai'
+
+const expect = chai.expect
+//单元测试
+{
+    const constructor = Vue.extend(Button)
+    const btn = new constructor({
+        propsData:{
+            icon:'settings'
+        }
+    })
+    btn.$mount('#test')
+    let useEl = btn.$el.querySelector('use')
+    let href = useEl.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-settings')
+
+}
+
+//icon
+{
+    const constructor = Vue.extend(Button)
+    const btn = new constructor({
+        propsData:{
+            icon:'settings',
+            loading:true
+        }
+    })
+    //内存中
+    btn.$mount()
+    let useEl = btn.$el.querySelector('use')
+    let href = useEl.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-circle-loading')
+
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+
+    const constructor = Vue.extend(Button)
+    const btn = new constructor({
+        propsData:{
+            icon:'settings'
+        }
+    })
+    btn.$mount(div)
+    let svg = btn.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+}
+
--- src/button.vue
+++ src/button.vue
@@ -1,22 +1,59 @@
 <template>
-    <button class="g-button">
+    <button class="g-button" 
+            :class="{[`icon-${iconPosition}`]:true}"
+            @click="$emit('click1')">
         <!-- <slot name="button_click"></slot> -->
 
         <!-- 不传icon则默认不显示svg -->
-        <svg v-if="icon" class="icon">
-                <use :xlink:href="`#icon-${icon}`"></use>
-        </svg>
-        <slot></slot>
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading" name="circle-loading"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
     </button>
 </template>
 
 <script>
     export default {
-        props:['icon']
+        //props:['icon','iconPosition']
+        //另外的写法
+        props:{
+            icon:{},
+            loading:{
+                type:Boolean,
+                default:false
+            },
+            iconPosition:{
+                type:String,
+                default:'left',
+
+                //既不是left 又不是right 
+                validator(v) {
+                    // if(v !== 'left' && v !== 'right'){
+                    //     return false;
+                    // }else{
+                    //     return true;
+                    // }
+
+                    //优化写法
+                    return v === 'left' || v ==='right';
+
+                }
+            }
+
+        }
     }
 </script>
 
 <style scoped lang="scss">
+    @keyframes spin {
+        0% {transform: rotate(0deg);}
+        100% {transform: rotate(360deg);}
+    }
+    .loading {
+        animation: spin 1s infinite linear;
+    }
+
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
@@ -24,7 +61,11 @@
         border-radius: var(--border-radius);
         border:1px solid var(--border-color);
         background: var(--button-bg);
-
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        // 对不齐 由display: inline-flex引起  解决方法是加下面的代码
+        vertical-align: middle;
 
         &:hover {
         border-color: var(--border-color-hover);
@@ -34,6 +75,24 @@
         }
         &:focus {
             outline: none;
+        }
+
+        > .content {
+            order: 2;
+        }
+        > .icon {
+            order: 1;
+            margin-right: .1em;
+        }
+        &.icon-right {
+            > .content {
+                order: 1;
+            }
+            > .icon {
+                order: 2;
+                margin-left: .1em;
+                margin-right: 0;
+            }
         }
     }
     
